e618e0c3d07536c031a6f38841c7cf6bbd615fce
Sergio laptop <sergiosantosmenaq@gmail.com>
2024-10-11 17:04:34 -0500

    clean index

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import DivisionOperation from "./operations/DivisionOperation";
-
-function makeSum() {
-    let sum = new DivisionOperation();
-    console.log("result", sum.run(10,5,0))
-}
-
-makeSum()
